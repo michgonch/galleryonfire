85033b6ac76d1bae061a1bf9470b669cb6cba17c
Michael Goncharov <michgonch@gmail.com>
2020-08-02 14:42:19 +0300

    Add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebae/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAMQX7kCeesxiVGOCSTua4dGzdgIgpmUgA",
+    authDomain: "galleryonfire-d1a63.firebaseapp.com",
+    databaseURL: "https://galleryonfire-d1a63.firebaseio.com",
+    projectId: "galleryonfire-d1a63",
+    storageBucket: "galleryonfire-d1a63.appspot.com",
+    messagingSenderId: "1001397922645",
+    appId: "1:1001397922645:web:f289bd0892c75c8e92dddc"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export default {projectFirestore, projectStorage};
