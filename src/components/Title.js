5593d10a0c2d80c2bf6969d0899218c874fcc801
Michael Goncharov <michgonch@gmail.com>
2020-08-03 13:34:24 +0300

    Add logo

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>Gallery on Fire</h1>
+      <h1>
+        <img src="/logo512.png" alt="logo" />
+        Gallery on Fire
+      </h1>
       <h2>Share your pictures</h2>
       <p>All images are property of their owners and responsibility of their uploaders.</p>
     </div>
