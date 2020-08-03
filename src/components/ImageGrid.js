91b210c1cbeac9e458a58558379d7c8b10a3a2cd
Michael Goncharov <michgonch@gmail.com>
2020-08-03 11:57:36 +0300

    Add Framer animations

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
+import { motion } from "framer-motion";
 
 const ImageGrid = ({ setSelectedImg }) => {
   const { docs } = useFirestore("images");
@@ -7,11 +8,17 @@ const ImageGrid = ({ setSelectedImg }) => {
   return (
     <div className="img-grid">
       {docs && docs.map(doc => (
-        <div className="img-wrap" key={doc.id}
+        <motion.div className="img-wrap" key={doc.id}
           onClick={() => setSelectedImg(doc.url)}
+          layout
+          whileHover={{ opacity: 1 }}
         >
-          <img src={doc.url} alt="uploaded" />
-        </div>
+          <motion.img src={doc.url} alt="uploaded"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1 }}
+          />
+        </motion.div>
       ))}
     </div>
   )
