b85042c948f2ddb493760f12e02e85e158882ce0
Michael Goncharov <michgonch@gmail.com>
2020-08-03 14:20:24 +0300

    Add disclaimer

--- src/components/Title.js
+++ src/components/Title.js
@@ -8,6 +8,7 @@ const Title = () => {
         Gallery on Fire
       </h1>
       <h2>Share your pictures</h2>
+      <p>All images are visible to anyone.</p>
       <p>All images are property of their owners and responsibility of their uploaders.</p>
     </div>
   )
